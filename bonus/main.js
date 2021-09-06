e7ffb24cb1d402f8fd02e0963b39cb42047e7143
Vlad <andreivladpaul@gmail.com>
2021-09-06 17:22:42 +0200

    Bonus

---
+++ bonus/main.js
@@ -0,0 +1,15 @@
+/* Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+ */
+Vue.config.devtools = true;
+
+const app = new Vue({
+    el : '#root',
+    data : {
+        message: 'Hello Folks',
+        images: './img/pc.jpg'
+
+    }
+
+});
